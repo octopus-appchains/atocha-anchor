1d9f347009d46aeef8a5a3332d91e4eec77b780f
Rivers Yang <rivers.yang@icloud.com>
2022-04-20 22:37:56 +0800

    Update cli samples.

--- scripts/cli-samples/deploy_to_mainnet.sh
+++ scripts/cli-samples/deploy_to_mainnet.sh
@@ -2,7 +2,7 @@
 set -e
 #
 export NEAR_ENV=mainnet
-export APPCHAIN_ID=xxxx
+export APPCHAIN_ID=atocha
 export REGISTRY_ACCOUNT_ID=octopus-registry.near
 export ANCHOR_ACCOUNT_ID=$APPCHAIN_ID'.'$REGISTRY_ACCOUNT_ID
 #
@@ -14,11 +14,13 @@ ARGS='{"appchain_id":"'$APPCHAIN_ID'","appchain_registry":"'$REGISTRY_ACCOUNT_ID
 near call $ANCHOR_ACCOUNT_ID new $ARGS --accountId $ANCHOR_ACCOUNT_ID --gas 200000000000000
 #
 ARGS='{"account_id":"'$ANCHOR_ACCOUNT_ID'","registration_only":null}'
-near call f5cfbc74057c610c8ef151a439252680ac68c6dc.factory.bridge.near storage_deposit $ARGS --accountId $ANCHOR_ACCOUNT_ID --deposit 1
+near call f5cfbc74057c610c8ef151a439252680ac68c6dc.factory.bridge.near storage_deposit $ARGS --accountId $ANCHOR_ACCOUNT_ID --deposit 0.0125
 #
 near call $ANCHOR_ACCOUNT_ID migrate_state '' --accountId $ANCHOR_ACCOUNT_ID --gas 200000000000000
 #
-near call $ANCHOR_ACCOUNT_ID set_token_price_maintainer_account '{"account_id":"octopus-registry.near"}' --accountId $ANCHOR_ACCOUNT_ID
-near call $ANCHOR_ACCOUNT_ID set_price_of_oct_token '{"price":"2740000"}' --accountId $ANCHOR_ACCOUNT_ID
+ARGS='{"account_id":"'$REGISTRY_ACCOUNT_ID'"}'
+near call $ANCHOR_ACCOUNT_ID set_token_price_maintainer_account $ARGS --accountId $ANCHOR_ACCOUNT_ID
 #
-near call $ANCHOR_ACCOUNT_ID change_minimum_delegator_deposit '{"value":"200000000000000000000"}' --accountId $ANCHOR_ACCOUNT_ID
+near call $ANCHOR_ACCOUNT_ID set_price_of_oct_token '{"price":"2740000"}' --accountId $REGISTRY_ACCOUNT_ID
+#
+near call $ANCHOR_ACCOUNT_ID pause_rewards_withdrawal '' --accountId $ANCHOR_ACCOUNT_ID
